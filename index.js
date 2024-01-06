70caf5aeff4830fc245af57d282f60bf25e71d62
Chanel-B <84045976+Chanel-B@users.noreply.github.com>
2024-01-06 22:52:35 +0100

    updated js, and winner text

---
+++ index.js
@@ -0,0 +1,21 @@
+function replaceTheDices() {
+
+    var number1 = Math.floor(Math.random() * 6) + 1;
+    if (number1 >= 1 && number1 <= 6) {
+        document.querySelector('.img1').setAttribute('src', './images/dice' + number1 + '.png');
+    }
+
+    var number2 = Math.floor(Math.random() * 6) + 1;
+    if (number2 >= 1 && number2 <= 6) {
+        document.querySelector('.img2').setAttribute('src', './images/dice' + number2 + '.png');
+    }
+
+    if (number1 == number2) {
+        document.querySelector('.winner-title').innerHTML = "Draw!!";
+    } else if (number1 > number2) {
+        document.querySelector('.winner-title').innerHTML = "Player 1 wins!!";
+    } else if (number1 < number2) {
+        document.querySelector('.winner-title').innerHTML = "Player 2 wins!!";
+    }
+
+}
